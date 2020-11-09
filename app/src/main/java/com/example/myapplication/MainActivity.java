8084bd6d0e962c710834b43904ba22207fddcd91
hector <hectorugc4@gmail.com>
2020-11-09 15:10:00 +0100

    Add more style and colors

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView my_image = (ImageView) findViewById(R.id.my_image);
         my_image.setImageResource(R.drawable.pull);
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+        Spinner dropdown1 = findViewById(R.id.spinner2);
+    //create a list of items for the dropdowns.
+        String[] taille = new String[]{"CH", "M", "L","XL"};
+        String[] colors = new String[]{"Blue", "Rouge", "Vert","Jaune"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, taille);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, colors);
+
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+        dropdown1.setAdapter(adapter1);
     }
 }
