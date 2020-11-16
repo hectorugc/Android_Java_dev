7f79a739bde97a00de4932db88715d30c36c3801
hector <hectorugc4@gmail.com>
2020-11-16 15:55:55 +0100

    TD2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,28 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);                                                                                                                                                                     
         setContentView(R.layout.activity_main);
-
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
         ImageView my_image = (ImageView) findViewById(R.id.my_image);
         my_image.setImageResource(R.drawable.pull);
         //get the spinner from the xml.
         Spinner dropdown = findViewById(R.id.spinner1);
         Spinner dropdown1 = findViewById(R.id.spinner2);
-    //create a list of items for the dropdowns.
+        //create a list of items for the dropdowns.
         String[] taille = new String[]{"CH", "M", "L","XL"};
         String[] colors = new String[]{"Blue", "Rouge", "Vert","Jaune"};
 
@@ -29,5 +32,9 @@ public class MainActivity extends AppCompatActivity {
 //set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
         dropdown1.setAdapter(adapter1);
+
+        Toast.makeText(this,R.string.ajouter_painer, Toast.LENGTH_SHORT).show();
+
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Pull.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+import java.util.ArrayList;
+
+public class Pull {
+
+    private String titre;
+    private String description;
+    private double prix;
+    private String img;
+
+}
